a96f6c404013bbc62c7cfa9d78c1d44519cb1571
kellerd <danny.keller@tc.gc.ca>
2015-11-26 15:47:49 -0500

    Add game

---
+++ Game/Domain.fs
@@ -0,0 +1,97 @@
+﻿namespace Domain
+
+module WarhammerDomain =
+    open FSharpx
+    open FSharpx.State
+
+    type Rule = { Name : string; Desc : string list }
+    type Tree<'a> = | Leaf of 'a | Branch of Tree<'a> list
+    type Model = { Name: string; Rules : Tree<Rule> }
+    type PlayerInfo = Model list
+    type Players = Player1 of PlayerInfo | Player2
+
+    type BoardInfo = {
+        Board : Model list
+        Players : Players list 
+        }
+
+    type MoveCapability = 
+        unit -> RuleResult 
+
+    /// A capability along with the position the capability is associated with.
+    /// This allows the UI to show information so that the user
+    /// can pick a particular capability to exercise.
+    and NextMoveInfo = {
+        // the pos is for UI information only
+        // the actual pos is baked into the cap.
+        posToPlay : Rule 
+        capability : MoveCapability }
+
+    /// The result of a move. It includes: 
+    /// * The information on the current board state.
+    /// * The capabilities for the next move, if any.
+    and RuleResult = 
+        | Player1ToMove of BoardInfo * NextMoveInfo list 
+        | Player2ToMove of BoardInfo * NextMoveInfo list 
+        | GameWon of BoardInfo * Player 
+        | GameTied of BoardInfo 
+
+    // Only the newGame function is exported from the implementation
+    // all other functions come from the results of the previous move
+    type WarhammerAPI  = 
+        {
+        newGame : MoveCapability
+        }
+
+module TickTacToeDomain =
+    type HorizPosition = Left | HCenter | Right
+    type VertPosition = Top | VCenter | Bottom
+    type CellPosition = HorizPosition * VertPosition 
+
+    type Player = PlayerO | PlayerX
+
+    type CellState = 
+        | Played of Player 
+        | Empty
+
+    type Cell = {
+        pos : CellPosition 
+        state : CellState 
+        }
+
+    /// Everything the UI needs to know to display the board
+    type DisplayInfo = {
+        cells : Cell list
+        }
+    
+    /// The capability to make a move at a particular location.
+    /// The gamestate, player and position are already "baked" into the function.
+    type MoveCapability = 
+        unit -> MoveResult 
+
+    /// A capability along with the position the capability is associated with.
+    /// This allows the UI to show information so that the user
+    /// can pick a particular capability to exercise.
+    and NextMoveInfo = {
+        // the pos is for UI information only
+        // the actual pos is baked into the cap.
+        posToPlay : CellPosition 
+        capability : MoveCapability }
+
+    /// The result of a move. It includes: 
+    /// * The information on the current board state.
+    /// * The capabilities for the next move, if any.
+    and MoveResult = 
+        | PlayerXToMove of DisplayInfo * NextMoveInfo list 
+        | PlayerOToMove of DisplayInfo * NextMoveInfo list 
+        | GameWon of DisplayInfo * Player 
+        | GameTied of DisplayInfo 
+
+    // Only the newGame function is exported from the implementation
+    // all other functions come from the results of the previous move
+    type TicTacToeAPI  = 
+        {
+        newGame : MoveCapability
+        }
+
+
---
+++ Game/Program.fs
@@ -0,0 +1,7 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open ConsoleApplication
+[<EntryPoint>]
+let main argv = 
+    ConsoleApplication.startGame()
+    0 // return an integer exit code
