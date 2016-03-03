de4501ea91c5091148267cefbf7e88432ca0735c
kellerd <danny.keller@tc.gc.ca>
2016-03-03 06:50:07 -0500

    Flesh out domain

---
+++ Game/ModelImplTest.fs
@@ -0,0 +1,24 @@
+﻿#if !INTERACTIVE 
+namespace Impl
+module ModelImplTest = 
+    open Domain.WarhammerDomain
+#endif
+    open System
+    let Termagant = 
+        {   Name="Termagant"; 
+            Id=Guid("666D7AF7-D74B-49B8-B7B9-EFFF44D77ACE");
+            Characteristic = 
+                [WeaponSkill    (CharacteristicValue 3);
+                BallisticSkill  (CharacteristicValue 3);
+                Strength        (CharacteristicValue 3);
+                Toughness       (CharacteristicValue 3);
+                Wounds          (CharacteristicValue 3);
+                Initiative      (CharacteristicValue 3);
+                Attacks         (CharacteristicValue 3);
+                Leadership      (CharacteristicValue 3);
+                InvSaves        (CharacteristicValue 3);
+                Saves           (CharacteristicValue 3)] 
+                |> List.map(fun x -> x.GetType().Name, x) 
+                |> Map.ofList;
+            Rules = [Description {Name = "Lurker"; Description = "Termagant Lurks when outside synapse"}];
+        }
