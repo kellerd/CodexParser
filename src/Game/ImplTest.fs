0e8864ae6dfd7632e3bbf75047bb9d0fbe3e9766
kellerd <daniel.allan.keller@gmail.com>
2016-05-05 19:52:24 -0400

    Characteristics as Value rules

--- src/Game/ImplTest.fs
+++ src/Game/ImplTest.fs
@@ -7,61 +7,56 @@ module ImplTest =
     let Termagant id = 
         { Name = "Termagant"
           Id = id
-          Characteristic = 
-              [ WeaponSkill(CharacteristicValue 3)
-                BallisticSkill(CharacteristicValue 3)
-                Strength(CharacteristicValue 3)
-                Toughness(CharacteristicValue 3)
-                Wounds(CharacteristicValue 3)
-                Initiative(CharacteristicValue 3)
-                Attacks(CharacteristicValue 3)
-                Leadership(CharacteristicValue 3)
-                InvSaves(CharacteristicValue 3)
-                Saves(CharacteristicValue 3) ]
-              |> List.map (fun x -> x.GetType().Name, x)
-              |> Map.ofList
           Base = BaseDiameter 25<mm>
           Rules = 
               [ Description { Name = "Lurker"
-                              Description = "Termagant Lurks when outside synapse" } ] }
+                              Description = "Termagant Lurks when outside synapse" } 
+                Value(WeaponSkill(CharacteristicValue 3) |> box)  
+                Value(BallisticSkill(CharacteristicValue 3) |> box) 
+                Value(Strength(CharacteristicValue 3) |> box      ) 
+                Value(Toughness(CharacteristicValue 3) |> box     ) 
+                Value(Wounds(CharacteristicValue 3) |> box        ) 
+                Value(Initiative(CharacteristicValue 3) |> box    ) 
+                Value(Attacks(CharacteristicValue 3) |> box       ) 
+                Value(Leadership(CharacteristicValue 3) |> box    ) 
+                Value(InvSaves(CharacteristicValue 3) |> box      ) 
+                Value(Saves(CharacteristicValue 3) |> box)
+                ]};
     
     let TermUnit = 
         { UnitModels = [ Termagant(Guid "666D7AF7-D74B-49B8-B7B9-EFFF44D77ACE") ]
           UnitName = "Termagaunts"
           Rules = 
-              [ DeactivatedUntilEndOfPhaseOnFirstUse(Rule(Function(Move 6.<inch>)));
-                DeactivatedUntilEndOfPhaseOnFirstUse(Rule(Function(Deploy))) ]
+              [ DeactivatedUntilEndOfPhaseOnFirstUse(Function(Move 6.<inch>));
+                DeactivatedUntilEndOfPhaseOnFirstUse(Function(Deploy)) ]
           Deployment = NotDeployed }
     
     let Hormagaunt id = 
         { Name = "Hormagaunt"
           Id = id
-          Characteristic = 
-              [ WeaponSkill(CharacteristicValue 3)
-                BallisticSkill(CharacteristicValue 3)
-                Strength(CharacteristicValue 3)
-                Toughness(CharacteristicValue 3)
-                Wounds(CharacteristicValue 3)
-                Initiative(CharacteristicValue 3)
-                Attacks(CharacteristicValue 3)
-                Leadership(CharacteristicValue 3)
-                InvSaves(CharacteristicValue 3)
-                Saves(CharacteristicValue 3) ]
-              |> List.map (fun x -> x.GetType().Name, x)
-              |> Map.ofList
           Base = BaseDiameter 25<mm>
           Rules = 
               [ Description { Name = "Lurker"
-                              Description = "Termagant Lurks when outside synapse" } ] }
+                              Description = "Termagant Lurks when outside synapse" }
+                Value(WeaponSkill(CharacteristicValue 3) |> box)    
+                Value(BallisticSkill(CharacteristicValue 3) |> box) 
+                Value(Strength(CharacteristicValue 3) |> box      ) 
+                Value(Toughness(CharacteristicValue 3) |> box     ) 
+                Value(Wounds(CharacteristicValue 3) |> box        ) 
+                Value(Initiative(CharacteristicValue 3) |> box    ) 
+                Value(Attacks(CharacteristicValue 3) |> box       ) 
+                Value(Leadership(CharacteristicValue 3) |> box    ) 
+                Value(InvSaves(CharacteristicValue 3) |> box      ) 
+                Value(Saves(CharacteristicValue 3) |> box) ]}
     
     let HormagauntUnit = 
         { UnitModels = [ Hormagaunt(Guid "666D7AF7-D74B-49B8-B7B9-EFFF44D77ACE") ]
           UnitName = "Hormagaunts"
           Rules = 
               [ Description { Name = "Bounding Leap"
-                              Description = "Run 3 extra inches" };
-                DeactivatedUntilEndOfPhaseOnFirstUse(Rule(Function(Move 6.<inch>)));
-                DeactivatedUntilEndOfPhaseOnFirstUse(Rule(Function(Deploy))) ]
+                              Description = "Run(CharacteristicValue 3) extra inches" };
+                DeactivatedUntilEndOfPhaseOnFirstUse(Function(Move 6.<inch>));
+                DeactivatedUntilEndOfPhaseOnFirstUse(Function(Deploy)) ]
           Deployment = NotDeployed }
     
     let initial = 
