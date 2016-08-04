3d4f4a7f8c3d7e7b4fbabcd474148ac94f504464
kellerd <danny.keller@tc.gc.ca>
2016-08-04 15:45:18 -0400

    Move some things around

---
+++ src/Game/Game.fs
@@ -0,0 +1,27 @@
+﻿namespace Domain
+module Game =
+    open WarhammerDomain
+    type EvalResult = 
+        | GameStateResult of GameState
+        | AskResult of Asker<EvalResult>
+    and MoveCapability = 
+        unit -> RuleResult
+    and RuleInfo = 
+        | UnitRuleInfo of UnitRuleInfo
+        | ModelRuleInfo of ModelRuleInfo 
+        | GameStateRuleInfo of Rule: Rule
+    and NextResult= 
+        | Next of (RuleInfo * MoveCapability) list
+        | Ask of Asker<RuleResult>
+    and RuleResult = 
+        | Player1ToMove of GameState * NextResult
+        | Player2ToMove of GameState * NextResult
+        | GameWon of GameState * Player 
+        | GameTied of GameState 
+
+    // Only the newGame function is exported from the implementation
+    // all other functions come from the results of the previous move
+    type WarhammerAPI  = 
+        {
+        NewGame : MoveCapability
+        }
---
+++ src/Game/Rule.fs
@@ -0,0 +1,117 @@
+﻿namespace Domain
+    open WarhammerDomain
+    
+    type Base =
+     | BaseDiameter of int<mm>
+     | ModelDimentions of Dimentions<mm>
+    type Phase = Begin | Movement | Psychic | Shooting | Assault | End
+    type Round = 
+        | Begin
+        | One    of Phase
+        | Two    of Phase
+        | Three  of Phase
+        | Four   of Phase
+        | Five   of Phase
+        | Six    of Phase
+        | Seven  of Phase
+        | End    
+    type CharacteristicValue = CharacteristicValue of int
+    type Characteristic = 
+        | WeaponSkill     of CharacteristicValue
+        | BallisticSkill  of CharacteristicValue
+        | Strength        of CharacteristicValue
+        | Toughness       of CharacteristicValue
+        | Wounds          of CharacteristicValue
+        | Initiative      of CharacteristicValue
+        | Attacks         of CharacteristicValue
+        | Leadership      of CharacteristicValue
+        | InvSaves        of CharacteristicValue
+        | Saves           of CharacteristicValue with
+        member this.ToString = toString this
+        static member FromString s = fromString<Characteristic> s
+
+    type DiceRoll = DiceRoll of int
+
+    type PlayerTurn = Top | Bottom 
+
+    type DeploymentType = 
+        | Destroyed
+        | OngoingReserves
+        | Reserves
+        | Deployed
+        | Start
+
+    type ModelRuleImpl = 
+        | MCharacteristic of Characteristic
+        | Melee of int * UnitGuid
+        | MeleeHits of int * int * UnitGuid
+        override this.ToString() = toString this
+        static member FromString s = fromString<ModelRuleImpl> s
+    and UnitRuleImpl = 
+        | Move of float<inch>
+        | DeploymentState of DeploymentType
+        | Deploy
+        | UCharacteristic of Characteristic
+        | SetCharacteristicUnit of string * Rule
+        override  this.ToString() = toString this
+        static member FromString s = fromString<UnitRuleImpl> s
+    and GameRuleImpl = 
+        | Noop
+        | EndPhase
+        | EndTurn
+        | EndGame
+        | PlayerTurn of PlayerTurn
+        | GameRound of Round
+        // | Deactivate of RuleApplication
+        | DeactivateUntil of LogicalExpression * RuleApplication
+        | Revert of RuleApplication
+        override this.ToString() = toString this
+        static member FromString s = fromString<GameRuleImpl> s
+    and LogicalExpression =
+        | Logical of LogicalExpression * LogicalOperator * LogicalExpression
+        | Not of LogicalExpression
+        | Rule of RuleApplication
+    and RuleApplication = 
+        | UnitRule of UnitRuleImpl * UnitGuid
+        | ModelRule of ModelRuleImpl * ModelGuid
+        | GameStateRule of GameRuleImpl
+        | Sequence of RuleApplication list
+    and Rule = 
+        | Function of RuleApplication
+        | UserActivated of Rule
+        | ActiveWhen of LogicalExpression * Rule
+        | Description of RuleDescription 
+        | Overwritten of Rule * Rule 
+
+    [<CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
+    [<AutoOpen>]
+    module Rule =
+        let overwrite newR x = Overwritten(newR,x)
+        let unoverwrite = function
+                                | Overwritten(newR,r) -> r
+                                | r -> r
+        let userActivated rule =
+            let rec userActivated' = function
+                    | ActiveWhen(logic,r) -> 
+                        ActiveWhen(logic,userActivated' r)
+                    | UserActivated(_) as rule -> rule
+                    | Description(_) as rule-> rule
+                    | Function(Sequence(r1::tail)) -> Function(Sequence(r1::GameStateRule(Revert(r1))::tail))
+                    | Function(Sequence([])) as rule -> rule
+                    | Function(r) -> Function(Sequence([r])) |> userActivated'
+                    | Overwritten(newRule,old) -> Overwritten(userActivated' newRule,old)
+            userActivated' rule |> UserActivated
+        let rec afterRunDeactivateUntil activatedWhen = function
+                | ActiveWhen(logic,rule) -> 
+                    ActiveWhen(logic,afterRunDeactivateUntil activatedWhen rule)
+                | UserActivated(rule)   -> afterRunDeactivateUntil activatedWhen rule |> UserActivated
+                | Description(_) as rule-> rule
+                | Function(Sequence(r1::tail)) -> 
+                    Function(Sequence(r1 :: (List.append tail [GameStateRule(DeactivateUntil(activatedWhen,r1))])))
+                | Function(Sequence([])) as rule -> rule
+                | Function(r) -> Function(Sequence([r])) |> afterRunDeactivateUntil activatedWhen
+                | Overwritten(newRule,old) -> Overwritten(afterRunDeactivateUntil activatedWhen newRule,old)
+        let onlyWhen l1 r1 = ActiveWhen(l1,r1)
+        let (<&>) l1 l2 = Logical(l1,And,l2)
+        let (<|>) l1 l2 = Logical(l1,And,l2)
+        let (<!>) l1 = Not(l1)
