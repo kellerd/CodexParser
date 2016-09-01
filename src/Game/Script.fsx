18bc4832416e58d5d1bb464bad7ba71d2d6921da
kellerd <danny.keller@tc.gc.ca>
2016-09-01 13:11:58 -0400

    Move test runner

---
+++ src/Game/Script.fsx
@@ -0,0 +1,21 @@
+﻿#r """..\..\packages\FSharpx.Collections\lib\net40\FSharpx.Collections.dll"""
+#load "Base\Equals.fs"
+    "Base\Map.fs"
+    "Base\Seq.fs"
+    "Base\Base.fs"
+    "Domain\Rule.fs"
+    "Domain\Board.fs"
+    "Domain\Asker.fs"
+    "Domain\Game.fs"
+    "Impl\GameState.fs"
+    "Impl\ImplTest.fs"
+    "Impl\RulesImpl.fs"
+    "Impl\GameLoop.fs"
+    "UI\ConsoleUi.fs"
+    "UI\Logging.fs"
+    
+let startGame() =
+    let api = GameImpl.GameLoop.api 
+    let loggedApi = Logging.Logger.injectLogging api
+    ConsoleUi.ConsoleWarhammer.startGame loggedApi 
+startGame()
