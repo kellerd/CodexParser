af3bc30e0f2e97781e721d9f5c3f9d1780b9666e
kellerd <danny.keller@tc.gc.ca>
2016-09-01 13:11:58 -0400

    Move test runner

--- src/Game/Program.fs
+++ src/Game/Program.fs
@@ -1,4 +1,8 @@
 ﻿[<EntryPoint>]
 let main argv = 
-    ConsoleApplication.ConsoleApplication.startGame()
+    let startGame() =
+        let api = GameImpl.GameLoop.api 
+        let loggedApi = Logging.Logger.injectLogging api
+        ConsoleUi.ConsoleWarhammer.startGame loggedApi 
+    startGame()
     0 // return an integer exit code
