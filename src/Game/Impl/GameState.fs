3e3fa383947e28e7f273bc2d918fec05ba65da43
kellerd <daniel.allan.keller@gmail.com>
2016-09-08 22:24:58 -0400

    Refactor tryUpdateRule

--- src/Game/Impl/GameState.fs
+++ src/Game/Impl/GameState.fs
@@ -3,6 +3,8 @@ module GameState =
     open FSharpx.Collections
     open Domain.Board
     open Microsoft.FSharp.Collections
+    open Domain
+
     let tryFindUnit gameState unitId = 
         gameState.Players |> List.tryPick (fun p -> p.Units |> Map.tryFind unitId)
     let tryFindUnitByModel gameState (model:Model) = 
@@ -25,11 +27,24 @@ module GameState =
         y :: (removeFirst pred xs)
     
     let def x _ = Some x
-    let defnot x _ = x
+    let defnot x _ = None
     let replaceUnitModels u (m:Model) nm = { u with UnitModels = Map.updateWith nm m.Id u.UnitModels } 
     let replacePlayerUnits p u nu = { p with Units = Map.updateWith nu u.Id p.Units }
     let replaceGameStatePlayers s p np = { s with Players = replace s.Players p np }
- 
+    let rec tryFindRule ruleApplication gameState =
+        match ruleApplication with
+        | UnitRule(rule,uId) -> 
+            let name = rule.ToString()
+            tryFindUnit gameState uId |> Option.bind(fun u -> u.Rules |> Map.tryFind name)
+        | ModelRule(rule,mId) ->
+            let name = rule.ToString()
+            tryFindModel gameState mId |> Option.bind(fun m -> m.Model.Rules |> Map.tryFind name)
+        | GameStateRule(rule) ->
+            let name = rule.ToString()
+            gameState.Rules |> Map.tryFind name
+        | Sequence(rule::_) ->
+            tryFindRule rule gameState 
+        | Sequence([]) -> None
     let replaceUnitModelsInPlayer p u m nm = def (replaceUnitModels u m nm) |> replacePlayerUnits p u
     let replaceUnitModelsInGameState s p u m nm = replaceUnitModelsInPlayer p u m nm |> replaceGameStatePlayers s p 
 
@@ -48,21 +63,24 @@ module GameState =
     let replaceRuleOnGameState  replace (gameState:GameState)  = 
         let newGameState = { gameState with Rules = gameState.Rules |> replace }
         newGameState
-    let tryReplaceRuleOnGameState name mapf gameState = 
-        replaceRuleOnGameState (Map.updateWith mapf name) gameState
+    let tryReplaceRuleOnGameState mapf rule gameState = 
+        let name = makeRule rule |> fst
+        replaceRuleOnGameState (Map.updateWith (mapf rule) name) gameState
     let replaceRuleOnUnit  (unit : Unit) replace gameState = 
         let newUnit = { unit with Rules = unit.Rules |> replace }
         updatePlayerInGameState unit (def newUnit) gameState
-    let tryReplaceRuleOnUnit name mapf uid gameState = 
+    let tryReplaceRuleOnUnit mapf rule uid gameState = 
+        let name = makeRule rule |> fst
         tryFindUnit gameState uid
-        |> Option.map (fun u -> replaceRuleOnUnit u (Map.updateWith mapf name) gameState)
+        |> Option.map (fun u -> replaceRuleOnUnit u (Map.updateWith (mapf rule) name) gameState)
         |> defaultArg <| gameState
     let replaceRuleOnModel  (model : Model) replace gameState = 
         let newmodel = { model with Rules = model.Rules |> replace }
         updateUnitInGameState model (def newmodel) gameState
-    let tryReplaceRuleOnModel name mapf mId gameState = 
+    let tryReplaceRuleOnModel mapf rule mId gameState = 
+        let name = makeRule rule |> fst
         tryFindModel gameState mId
-        |> Option.map (fun m -> replaceRuleOnModel m.Model (Map.updateWith mapf name) gameState)
+        |> Option.map (fun m -> replaceRuleOnModel m.Model (Map.updateWith (mapf rule) name) gameState)
         |> defaultArg <| gameState
     let forAllModels f newUnit gameState = 
         [ for m in newUnit.UnitModels do
